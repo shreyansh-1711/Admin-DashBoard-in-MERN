cb569e60e4a946d3070e28cdf6a0737ff6dec65f
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-28 23:49:57 +0530

    seach icon inserted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImage from "assets/shreyansh.jpg";
-import { useTheme } from "@nivo/core";
+import { useTheme, Toolbar , AppBar, IconButton, InputBase} from "@mui/material";
 
 
 const Navbar = () => {
@@ -26,6 +26,27 @@ const Navbar = () => {
   boxShadow : "none",
   }}
   >
+    <Toolbar sx={{justifyContent: "space-between"}}>
+        {/* Left Side  */}
+        <FlexBetween>
+            <IconButton onClick={()=> console.log('open/close sidebar')}>
+                <MenuIcon />
+            </IconButton>
+            <FlexBetween
+                backgroundColor={theme.palette.background.alt}
+                borderRadius="9px"
+                gap="3rem"
+                p="0.1rem 1.5rem"
+            >
+                <InputBase placeholder="Search..." />
+                <IconButton>
+                    <Search />
+                </IconButton>
+
+            </FlexBetween>
+        </FlexBetween>
+
+    </Toolbar>
 
   </AppBar>
 };
