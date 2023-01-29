3452f73f1d0e2f7a70c1e3f5a4dfb715106a411f
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-30 00:20:47 +0530

    user schema

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,8 @@
 import express, { Router } from 'express';
+import { getUser } from "../controllers/general.js"
 
 const router = express.Router();
+
+router.get("/user/:id", getUser);
 
 export default router;
