5926d635e62c192a36ffa58fbbe90824a3287e11
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-30 23:47:35 +0530

    api fetched using createApi

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -4,10 +4,17 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  // Grab information from redux toolkit 
+  const userId = useSelector((state)=> state.global.userId);
+  const { data } = useGetUserQuery(userId);
+  console.log('data', data);
+
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,6 +1,6 @@
 import { CreateApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const api = createAPI({
+export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
     tagTypes: ["User"],
