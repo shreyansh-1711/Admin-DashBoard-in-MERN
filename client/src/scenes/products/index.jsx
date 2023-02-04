56f83088d7bafff58ee0118f4e300ef62ce8fec6
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-02-04 12:17:55 +0530

    server side pagination

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -88,7 +88,7 @@ const Product = ({
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
-
+ 
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products." />
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 
 export const getProducts = async (req, res) => {
@@ -30,8 +31,48 @@ export const getProducts = async (req, res) => {
 
 export const getCustomers = async (req, res) => {
   try {
-    const customers = await User.find({ role : "user"}).select("-password");
+    const customers = await User.find({ role: "user" }).select("-password");
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+    const generalSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+      };
+      return sortFormatted;
+    }
+
+    const sortFormatted = Boolean(sort) ? generalSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [// $or allows us to search for multiple fields
+
+        { cost: { $regex: new RegExp(search, "i") } },  //cost is being searched
+        { userId: { $regex: new RegExp(search, "i") } },
+
+      ],
+    })
+
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" }
+    })
+
+
+    res.status(200).json({
+      transactions,
+      total
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
